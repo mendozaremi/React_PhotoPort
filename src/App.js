66b83f717f5434ba5f5e6451d5e741ed60b92a5d
mendozaremi <mendozaremi@gmail.com>
2020-06-24 14:47:11 -0400

    navbar 50% complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SimpleMenu from './nav'
+import NavBar from './NavBar'
 
 const App = () =>{
   return (
     <div className="App">
-      <SimpleMenu/>
-    <h1>React Photography Portfolio</h1>
+      <NavBar/>
     </div>
   );
 }
