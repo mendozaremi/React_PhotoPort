8cd63edc3ed03d5aa248d07741dbbce754547d62
mendozaremi <mendozaremi@gmail.com>
2020-06-29 21:09:52 -0400

    fixing grid layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar'
 import Album from './Components/PhotoAlbum'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+ gridContainer: {
+   paddingLeft: '20px',
+   paddingRight: '20px',
+   paddingTop: '100px'
+ }
+});
 
 const App = () =>{
+  const classes = useStyles();
   return (
     <div className="App">
-      <NavBar/>
-      <Album/>
+       <NavBar/>
+      <Grid container spacing={4} className={classes.gridContainer}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Album/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
