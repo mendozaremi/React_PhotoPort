9131a18ce4c1893c883d9fadd8bc1103b0f00ab4
mendozaremi <mendozaremi@gmail.com>
2020-06-30 15:57:32 -0400

    Finished Card layout added photos

--- src/App.js
+++ src/App.js
@@ -5,24 +5,12 @@ import Album from './Components/PhotoAlbum'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
- gridContainer: {
-   paddingLeft: '20px',
-   paddingRight: '20px',
-   paddingTop: '100px'
- }
-});
 
 const App = () =>{
-  const classes = useStyles();
   return (
     <div className="App">
        <NavBar/>
-      {/* <Grid container spacing={4} className={classes.gridContainer}>
-        <Grid tem xs={12} sm={6} md={4}> */}
           <Album/>
-        {/* </Grid>
-      </Grid> */}
     </div>
   );
 }
--- src/Components/PhotoAlbum.js
+++ src/Components/PhotoAlbum.js
@@ -11,6 +11,7 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import Color from 'color';
 
 const useStyles = makeStyles({
   root: {
@@ -27,18 +28,34 @@ const useStyles = makeStyles({
     paddingBottom: '100px',
     paddingLeft: '20px',
     paddingRight: '20px',
-  },
+},
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '100%', // 16:9
+    paddingBottom: '40%'
   },
+  card: ({ color }) => ({
+    minWidth: 256,
+    boxShadow: 'none',
+    '&:hover': {
+      boxShadow: `0 6px 12px 0 ${Color(color)
+        .rotate(-12)
+        .darken(0.2)
+        .fade(0.5)}`,
+        transition: '0.2s',
+        '&:hover': {
+          transform: 'scale(1.1)',
+      },
+    },
+    
+  }),
 });
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      {'Copyright © Remi Mendoza '}
+      <Link color="inherit" href="https://mendozaremi.github.io/portfolio/">
+        Visit My Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -50,182 +67,133 @@ export default function Album() {
 
   return (
     <React.Fragment>
-    
-    <main>
-        <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={4}>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://imgur.com/W6hXRKe"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://imgur.com/UcEuEph"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    className={classes.cardMedia}
-                    image="https://imgur.com/a/synLW8S"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    className={classes.cardMedia}
-                    image="/public/images/Cuba1.jpg"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
-      {/* End footer */}
+      <main>
+          <Container className={classes.cardGrid} maxWidth="md">
+            {/* FIRST IMG */}
+            <Grid container spacing={8}>
+        
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/QCRdpZc.jpg"
+                    />
+                  </Card>
+                </Grid>
+                {/* SECOND IMG */}
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/yxAq4Bd.jpg"
+                    />
+                  </Card>
+                </Grid>
+                {/* THIRD IMG */}
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/0rCL5tW.jpg"
+                    />
+                  </Card>
+                </Grid>
+                {/* FOURTH IMG */}
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/ONzJjZ1.jpg"
+                    />
+                  </Card>
+                </Grid>
+                {/* FIFTH IMG */}
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/sL5cVmz.jpg"
+                    />
+                  </Card>
+                </Grid>
+                {/* SIXTH IMG */}
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/iGuoAbO.jpg"
+                    />
+                  </Card>
+                </Grid>
+                   {/* SEVENTH IMG */}
+                   <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/BCfyZ3n.jpg"
+                    />
+                  </Card>
+                </Grid>
+                   {/* EIGHT IMG */}
+                   <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/W6hXRKe.jpg"
+                    />
+                  </Card>
+                </Grid>
+                   {/* NINTH IMG */}
+                   <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/UcEuEph.jpg"
+                    />
+                  </Card>
+                </Grid>
+                    {/* TENTH IMG */}
+                    <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/2Bi8OBk.jpg"
+                    />
+                  </Card>
+                </Grid>
+                    {/* ELEVENTH IMG */}
+                    <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/LrcJl9N.jpg"
+                    />
+                  </Card>
+                </Grid>
+                    {/* TWENTH IMG */}
+                    <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://i.imgur.com/tLQIT2f.jpg"
+                    />
+                  </Card>
+                </Grid>
+                
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <footer className={classes.footer}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Something here to give the footer a purpose!
+          </Typography>
+          <Copyright />
+        </footer>
+        {/* End footer */}
     </React.Fragment>
     
   );
