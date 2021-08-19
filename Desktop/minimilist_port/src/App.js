5dad804d9ba062d202cf7f6c8b773bbe6136a823
mendozaremi <mendozaremi@gmail.com>
2021-08-19 11:06:46 -0400

    completed Navbar

---
+++ Desktop/minimilist_port/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+     {/* Welcome*/} 
+      {/* About me*/} 
+       {/* Projectx*/} 
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/minimilist_port/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import scrollTo from "gatsby-plugin-smoothscroll"
+import "../main.scss"
+
+const Navbar = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="navbar-wrapper">
+          <div
+            role="button"
+            onClick={() => scrollTo("#home")}
+            className="name"
+            tabIndex={0}
+          >
+            Remi Mendoza
+          </div>
+          <div className="links-wrapper">
+            <button onClick={() => scrollTo("#work")}>Work</button>
+            <button onClick={() => scrollTo("#about")}>About</button>
+            <button onClick={() => scrollTo("#contact")}>Contact</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
